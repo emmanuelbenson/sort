a4cdec36d6f62c5ce122772ee7a7cd732ef4e271
Emmanuel Benson <emmanuel.benson@konga.com>
2022-03-18 14:36:18 +0100

    Add Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello Typescript!");
+export class Sorter {
+    constructor(public collection: number[]) {}
+
+    sort(): void {
+
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0, 50]);
+sorter.sort();
+console.log(sorter.collection);
